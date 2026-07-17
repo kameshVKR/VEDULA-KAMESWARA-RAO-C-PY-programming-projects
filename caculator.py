e54ab17c2b2014fb738d9f64ba24125b000f2295
VEDULA KAMESWARA RAO <vedulakameshvkr@gmail.com>
2026-07-17 14:49:22 +0530

    Update caculator.py

---
+++ caculator.py
@@ -0,0 +1,13 @@
+operator = input("enter your operator(+, -, *, /): ")
+first_value = int(input("enter your first value: "))
+second_value = int(input("enter your second value: "))
+if operator == '+':
+ print(first_value + second_value)
+elif operator == '-':
+  print(first_value - second_value)
+elif operator == '*':
+  print(first_value * second_value)
+elif operator == '/':
+  print(first_value / second_value)
+else : print("error999+: invalid input")
+
